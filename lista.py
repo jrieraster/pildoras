f3e381d01ffbd00406378e416e82151a4fd69232
Jrieraster <jrieraster@gmail.com>
2020-03-21 20:08:20 -0300

    Llegamos hasta Tuplas

--- lista.py
+++ lista.py
@@ -1,21 +1,47 @@
 miLista=["Maria", "Pepe", "MArta", "Antonio"]
 
-print(miLista[1:])
+print(miLista[:]) #Imprime la lista entera
 
-miLista.append("Sandra")
+print(miLista[3]) #Imprime el indice 3 de la lista
 
-print(miLista[1:])
+print(miLista[0:2]) #Imprime desde el indice 0 (incluido) hasta el indice 2 (no incluido)
+
+print(miLista[1:]) #Imprime a partir del indice 1
+
+print(miLista[:2]) #Imprime hasta el indice 2 (no incluido)
+
+
+miLista.append("Sandra") #Agregamos un indice al final
+
+print(miLista[:])
 
 miLista.insert(2,"Tito") # Insertamos en la posición 3 a Tito
 
-print(miLista[1:])
+print(miLista[:])
 
 miLista.extend(["Tati","Toto","Teto"]) # Concatena otra lista
 
 print(miLista[:]) #Imprime la lista entera
 
-print(miLista.index("Tito"))
+print(miLista.index("Tito")) # Imprime el número de indice de Tito
 
 print(miLista.index("Teto")) # Imprime el número de indice de Teto
 
 print("Tati" in miLista) # Imprime si esta o no en la lista 
+
+myList=["Maria", 5, True, 78.35]
+
+print(myList[:])
+
+myList.remove(True) # Indico que elemneto deseo eliminar
+
+print(myList[:])
+
+myList.pop() # Elimina el ultimo elemento de la lista
+
+print(myList[:])
+
+totalList=miLista+myList
+
+print(totalList[:])
+
---
+++ tupla_var.py
@@ -0,0 +1,7 @@
+mitupla=("juan", 5, 3, 1995)
+nombre, dia, mes, anio=mitupla # Le asigno variables a los valores de la tupla. Esto es "Desempaquetado de tupla"
+
+print(nombre)
+print(anio)
+
+print(mitupla.index(5))
