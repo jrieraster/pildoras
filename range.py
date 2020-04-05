c8d71634a0627c437a70e27cb7e1986fa2877b2c
Javi <jrierasterggmail.com>
2020-04-05 12:19:33 -0300

    Vamos por While

---
+++ range.py
@@ -0,0 +1,11 @@
+for i in range(2):
+    print(f"Valor de la variable = {i}")
+
+
+for i in range(8,12):
+    print(f"Valor de la variable = {i}")
+
+
+
+for i in range(5,50,6):
+    print(f"Valor de la variable = {i}")
