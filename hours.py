d8956a5f0954e6e886b62df1aeb13b1641beda6a
Jrieraster <jrieraster@gmail.com>
2020-05-07 17:58:10 -0300

    Seguimos avanzando

---
+++ hours.py
@@ -0,0 +1,8 @@
+# Esta primera línea se proporciona para usted
+
+hrs = float(input("Introduce horas:"))
+tarifa = float(input("Introduce Tarifa por hora:"))
+
+paga = hrs * tarifa
+
+print("Su salario bruto es: ",paga)
