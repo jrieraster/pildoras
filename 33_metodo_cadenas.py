ce4a3c1bc3189a2489fc68eb8ca253c139155577
Javi <jrieraster@gmail.com>
2020-07-06 01:20:10 -0300

    Ejercicio cadenas

--- 33_metodo_cadenas.py
+++ 33_metodo_cadenas.py
@@ -8,17 +8,38 @@
 # print("El nombre es: ", nombreUsuario.capitalize())
 
 
-edad=input("Introduce la edad: ")
+# edad=input("Introduce la edad: ")
 
-while(edad.isdigit()==False):
-    print("Por favor introduce un valor numerico")
-    edad=input("Introduce la edad: ")
-
-
-if (int(edad)<18 or int(edad)>100):
-    print("No puede pasar")
-else:
-    print("Puedes pasar")
+# while(edad.isdigit()==False):
+#     print("Por favor introduce un valor numerico")
+#     edad=input("Introduce la edad: ")
 
 
-print(edad.isdigit())
+# if (int(edad)<18 or int(edad)>100):
+#     print("No puede pasar")
+# else:
+#     print("Puedes pasar")
+
+
+# print(edad.isdigit())
+
+
+email=input("Introduce tu email: ")
+
+while(email.count("@")!=1):
+    print("Por favor introduce un correo correcto")
+    email=input("Introduce tu email: ")
+
+while(email.count(".")==0):
+    print("Por favor introduce un correo correcto")
+    email=input("Introduce tu email: ")
+
+while(email.endswith("@")==True):
+    print("Por favor introduce un correo correcto")
+    email=input("Introduce tu email: ")
+
+while(email.startswith("@")==True):
+    print("Por favor introduce un correo correcto")
+    email=input("Introduce tu email: ")
+print("La direccion de correo es correcta")
+
